e8529d7fb68fbcdb251fbbf1e57192fe7af03665
Ivan Masmitjà <imasmitja@gmail.com>
2016-11-22 10:22:41 +0100

    New NUnit test project

---
+++ NPocoSamples/Properties/AssemblyInfo.cs
@@ -0,0 +1,16 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("NPocoSamples")]
+[assembly: AssemblyDescription("NPoco v3 Samples")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Ivan Masmitjà Dagas")]
+[assembly: AssemblyProduct("NPocoSamples")]
+[assembly: AssemblyCopyright("Copyright ©  Ivan Masmitja  2016")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: AssemblyVersion("0.1.0.0")]
+[assembly: AssemblyFileVersion("0.1.0.0")]
---
+++ NPocoSamples/QueryTests.cs
@@ -0,0 +1,15 @@
+﻿using NUnit.Framework;
+
+namespace NPocoSamples
+{
+    [TestFixture]
+    public class QueryTests
+    {
+        [Test]
+        public void TestMethod()
+        {
+            // TODO: Add your test code here
+            Assert.Pass("Your first passing test");
+        }
+    }
+}
