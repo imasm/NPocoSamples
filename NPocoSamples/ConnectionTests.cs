80279ce12f244e9e33e648272c94a59773498528
Ivan Masmitjà <imasmitja@gmail.com>
2016-11-22 11:11:12 +0100

    Add connection tests
    Open NPoco database using most common constructors

---
+++ NPocoSamples/ConnectionTests.cs
@@ -0,0 +1,40 @@
+﻿using NPoco;
+using NUnit.Framework;
+
+namespace NPocoSamples
+{
+    [TestFixture]
+    public class ConnectionTests
+    {
+        [Test]
+        public void TestConnect_WithConnectionName()
+        {
+            using (var db = new Database(DbInfo.Name))
+            {
+                int result = db.ExecuteScalar<int>("SELECT 1");
+                Assert.AreEqual(1, result);
+            }
+        }
+
+
+        [Test]
+        public void TestConnect_WithConnectionStringAndProviderName()
+        {
+            using (var db = new Database(DbInfo.ConnecionString, DbInfo.ProviderName))
+            {
+                int result = db.ExecuteScalar<int>("SELECT 1");
+                Assert.AreEqual(1, result);
+            }
+        }
+
+        [Test]
+        public void TestConnect_WithConnectionStringAndDatabaseType()
+        {
+            using (var db = new Database(DbInfo.ConnecionString, DbInfo.DatabaseType))
+            {
+                int result = db.ExecuteScalar<int>("SELECT 1");
+                Assert.AreEqual(1, result);
+            }
+        }
+    }
+}
---
+++ NPocoSamples/DbInfo.cs
@@ -0,0 +1,16 @@
+﻿using NPoco;
+
+namespace NPocoSamples
+{
+    public static class DbInfo
+    {
+        public const string Name = "Northwind";
+
+        public const string ConnecionString =
+            @"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=NORTHWIND;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=True;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
+
+        public const string ProviderName = "System.Data.SqlClient";
+
+        public static readonly DatabaseType DatabaseType = DatabaseType.SqlServer2012;
+    }
+}
