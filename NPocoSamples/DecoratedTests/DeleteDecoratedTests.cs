cba19c5eb6293da3cb683f65ca306293dfc3268a
Ivan Masmitjà <imasmitja@gmail.com>
2016-11-23 14:49:01 +0100

    Add select queries with sql and delete samples

---
+++ NPocoSamples/DecoratedTests/DeleteDecoratedTests.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using NPoco;
+using NPocoSamples.Common;
+using NPocoSamples.DecoratedModels;
+using NUnit.Framework;
+
+namespace NPocoSamples.DecoratedTests
+{
+    [TestFixture]
+    public class DeleteDecoratedTests: BaseTests
+    {
+        [OneTimeSetUp]
+        [OneTimeTearDown]
+        public void DeleteAddedCustomer()
+        {
+            Output("Delete added customer");
+            using (var db = new Database(DbInfo.Name))
+            {
+                db.BeginTransaction();
+                db.ExecuteScalar<int>("delete from customers where customerId = 'DEMO'");
+                db.CompleteTransaction();
+            }
+        }
+
+        [SetUp]
+        public void InsertCustomerToUpdate()
+        {
+            Output("Insert customer to delete");
+            using (var db = new Database(DbInfo.Name))
+            {
+                db.BeginTransaction();
+                var customer = new CustomerDecorated()
+                {
+                    CustomerId = "DEMO",
+                    CompanyName = "My company",
+                    ContactTitle = "Mr",
+                    ContactName = "John Smith",
+                };
+                db.Insert(customer);
+                db.CompleteTransaction();
+            }
+        }
+
+        [Test]
+        public void Delete_Object()
+        {
+            using (var db = new TestDatabase(DbInfo.Name))
+            {
+                db.BeginTransaction();
+                var customer = db.SingleById<CustomerDecorated>("DEMO");
+                int deleted = db.Delete(customer);
+                db.CompleteTransaction();
+                Assert.That(deleted, Is.EqualTo(1));
+            }
+
+            AssertIsDeleted("DEMO");
+        }
+
+        [Test]
+        public void Delete_ByPrimaryKey()
+        {
+            using (var db = new TestDatabase(DbInfo.Name))
+            {
+                db.BeginTransaction();
+                int deleted = db.Delete<CustomerDecorated>((object)"DEMO");
+                db.CompleteTransaction();
+                Assert.That(deleted, Is.EqualTo(1));
+            }
+
+            AssertIsDeleted("DEMO");
+        }
+
+        [Test]
+        public void Delete_ByWhere()
+        {
+            using (var db = new TestDatabase(DbInfo.Name))
+            {
+                db.BeginTransaction();
+                int deleted = db.Delete<CustomerDecorated>("where CustomerId = @0", "DEMO");
+                db.CompleteTransaction();
+                Assert.That(deleted, Is.EqualTo(1));
+            }
+
+            AssertIsDeleted("DEMO");
+        }
+
+
+        private void AssertIsDeleted(string customerId)
+        {
+            using (var db = new TestDatabase(DbInfo.Name))
+            {
+                var customer = db.SingleOrDefaultById<CustomerDecorated>(customerId);
+                Assert.That(customer, Is.Null);
+            }
+        }
+        
+    }
+}
---
+++ NPocoSamples/DecoratedTests/SqlQueryDecoratedTests.cs
@@ -0,0 +1,94 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using NPoco;
+using NPocoSamples.Common;
+using NPocoSamples.DecoratedModels;
+using NUnit.Framework;
+
+namespace NPocoSamples.DecoratedTests
+{
+    [TestFixture]
+    public class SqlQueryDecoratedTests : BaseTests
+    {
+        [Test]
+        public void Test_First()
+        {
+            using (var db = new TestDatabase(DbInfo.Name))
+            {
+                ProductDecorated result = db.First<ProductDecorated>("select * from products where ProductId = @0", 1);
+                Assert.That(result, Is.Not.Null);
+                AssertIsProduct1(result);
+            }
+        }
+
+        [Test]
+        public void Test_First_Exception()
+        {
+            Assert.That(() =>
+            {
+                using (var db = new TestDatabase(DbInfo.Name))
+                {
+                    db.First<ProductDecorated>("select * from products where ProductId = @0", 9999);
+                }
+            }, Throws.Exception);
+        }
+
+        [Test]
+        public void Test_FirstOrDefault()
+        {
+            using (var db = new TestDatabase(DbInfo.Name))
+            {
+                var result = db.FirstOrDefault<ProductDecorated>("select * from products where ProductId = @0", 9999);
+                Assert.That(result, Is.Null);
+            }
+        }
+
+        [Test]
+        public void Test_Fetch()
+        {
+            using (var db = new TestDatabase(DbInfo.Name))
+            {
+                List<ProductDecorated> result = db.Fetch<ProductDecorated>("select * from products");
+                Output(result);
+
+                Assert.That(result.Count, Is.EqualTo(77));
+                AssertIsProduct1(result.First(x => x.CategoryId == 1));
+            }
+        }
+
+        [Test]
+        public void Test_Query()
+        {
+            using (var db = new TestDatabase(DbInfo.Name))
+            {
+                IEnumerable<ProductDecorated> result = db.Query<ProductDecorated>("select * from products");
+                Output(result);
+            }
+        }
+        
+        [Test]
+        public void Test_Query_WithFilter()
+        {
+            using (var db = new Database(DbInfo.Name))
+            {
+                List<ProductDecorated> result = db.Query<ProductDecorated>("select * from products where CategoryId=@0", 1)
+                    .ToList();
+
+                Output(result);
+
+                Assert.That(result.Count, Is.EqualTo(12));
+                Assert.That(result.All(x => x.CategoryId == 1), Is.True);
+            }
+        }
+
+      
+        private void AssertIsProduct1(ProductDecorated product)
+        {
+            Assert.That(product.ProductId, Is.EqualTo(1));
+            Assert.That(product.ProductName, Is.EqualTo("Chai"));
+            Assert.That(product.CategoryId, Is.EqualTo(1));
+            Assert.That(product.UnitPrice.GetValueOrDefault(), Is.EqualTo(18.0m));
+        }
+
+    }
+}
